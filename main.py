49279da9a1cc52562f34032761f63144e5d18b4e
KolwaBrad <duncan.bradley@strathmore.edu>
2023-10-01 17:18:06 +0300

    updated main function

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import os
+import pandas as pd
+import sys
 
 
-# Press the green button in the gutter to run the script.
+
+# Load the JSONL data
+data_folder = '1.1/data'
+
+pivotLanguage = os.path.join(data_folder, 'en-US.jsonl')
+
+english = pd.read_json(pivotLanguage, lines=True)
+english = english[['id', 'utt', 'annot_utt']]
+
+def generate_excel(language):
+    if language == 'en-US.jsonl':
+        return
+    try:
+        json_path = os.path.join(data_folder, language)
+        df = pd.read_json(json_path, lines=True)
+        df = df[['id', 'utt', 'annot_utt']]
+        mergedPdf = pd.merge(english, df, on='id')
+        output_folder = '1.1/excel'
+        os.makedirs(output_folder, exist_ok=True)
+        output_file_path = os.path.join(output_folder, f'en-{language[:2]}.xlsx')
+        mergedPdf.to_excel(output_file_path, index=True)
+        print(f"Finished processed {language}")
+    
+    except Exception as e:
+        print(f"Error in file {language}: {e}")
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    language = sys.argv[1]
+    generate_excel(language)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
